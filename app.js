2855691c9ada93f0f95e8999bdefa621420da653
Dominik Kowalski <dominikkowalski331@gmail.com>
2023-10-25 22:57:50 +0200

    Created working fetch()

--- app.js
+++ app.js
@@ -4,45 +4,19 @@ const searchBtn = document.querySelector("#search-btn");
 let citySearchInput = document.querySelector("#citySearch");
 
 searchBtn.addEventListener("click", async (e) => {
-  let citySearch = document.querySelector("#citySearch").value;
-
-  const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${citySearch}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-    },
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${citySearchInput.value}`
+  );
+  const data = await response.json();
+  console.log(data);
 });
 
 citySearchInput.addEventListener("keypress", async (key) => {
   if (key.key === "Enter") {
-    let citySearch = document.querySelector("#citySearch").value;
-
-    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${citySearch}`;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${citySearchInput.value}`
+    );
+    const data = await response.json();
+    console.log(data);
   }
 });
