1879d654e4d1d293747affc1b7840e13cda53fed
Dominik Kowalski <dominikkowalski331@gmail.com>
2023-12-03 19:44:19 +0100

    API info displays correctly

--- app.js
+++ app.js
@@ -6,56 +6,66 @@ let citySearchInput = document.querySelector("#citySearch");
 searchBtn.addEventListener("click", async (e) => {
   await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${citySearchInput.value}&days=1&aqi=no&alerts=no`)
 
-    .then((data) => {
-      if (!data.ok) throw new Error("Invalid response");
-      return data.json();
-    })
+  .then((data) => {
+    if (!data.ok) throw new Error("Invalid response");
+    return data.json();
+  })
 
-    .then((dataobject) => {
+  .then((dataobject) => {
 
-      document.querySelector("#locname").textContent = dataobject.location.name;
+    document.querySelector("header").style.marginTop = 0;
 
-      document.querySelector("#mainimg").src = dataobject.current.condition.icon;
+    document.querySelector(".weather-info").style.display = "flex"
 
-      document.querySelector("#temp").textContent = dataobject.current.temp_c + "°C";
+    document.querySelector(".container").style.width = "400px"
 
-      document.querySelector("#currtemp").textContent = dataobject.current.temp_c + "°";
+    document.querySelector(".container").style.height = "460px"
 
-      document.querySelector("#minimainimg").src = dataobject.current.condition.icon;
+    document.querySelector("#locname").textContent = dataobject.location.name;
 
-      let forecasttemp = document.querySelectorAll(".forecasttemp");
-      let forecasttime = document.querySelectorAll(".forecasttime");
-      let forecastimg = document.querySelectorAll(".forecastimg");
+    document.querySelector("#mainimg").src = dataobject.current.condition.icon;
 
-      forecasttemp.forEach((element) => {
-        for (let i = 0; i < 4; i++) {
-          forecasttemp[i].textContent =
-            Math.round(dataobject.forecast.forecastday[0].hour[i].temp_c) + "°";
-          
-          forecastimg[i].src = dataobject.forecast.forecastday[0].hour[i].condition.icon;
+    document.querySelector("#temp").textContent = dataobject.current.temp_c + "°C";
+
+    document.querySelector("#currtemp").textContent = dataobject.current.temp_c + "°";
+
+    document.querySelector("#minimainimg").src = dataobject.current.condition.icon;
+
+    let forecasttemp = document.querySelectorAll(".forecasttemp");
+    let forecasttime = document.querySelectorAll(".forecasttime");
+    let forecastimg = document.querySelectorAll(".forecastimg");
+    let currTime = Number(dataobject.current.last_updated.slice(11, 13))
+
+    forecasttime.forEach((element) => {
+      for (let i = 0; i < 4; i++) {
+        let forTime = String(currTime + 1 + i);
+        forecasttime[i].textContent = forTime;
         }
-      });
+      }),
 
-      forecasttime.forEach((element) => {
-        for (let i = 0; i < 4; i++) {
-          let currTime = dataobject.current.last_updated.slice(11, 13)
-          if(currTime.includes("0")) {
-            Number(currTime) + i + 1;
-            forecasttime[i].textContent = currTime;
-          }else {
-            
-          }
-          // let currTime = Number(dataobject.current.last_updated.slice(11, 13) + i + 1)
-          // let forTime = "0" + String(currTime);
-          // forecasttime[i].textContent = forTime;
-        }
+    forecasttemp.forEach((element) => {
+      for (let i = 0; i < 4; i++) {
+        forecasttemp[i].textContent =
+          Math.round(dataobject.forecast.forecastday[0].hour[currTime + 1 + i].temp_c) + "°";
+        
+        forecastimg[i].src = dataobject.forecast.forecastday[0].hour[currTime + 1 + i].condition.icon; 
       }
-      );
+    });
+
+    if(dataobject.current.is_day == 0) {
+      document.querySelector(".container").style.backgroundImage = "url('Images/nightSkybg.avif')"
+    }else if(dataobject.current.is_day == 1 && dataobject.current.condition.text.includes("rain", "snow")) {
+      document.querySelector(".container").style.backgroundImage = "url('https://images.unsplash.com/photo-1595949389862-dcc5a5945217?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JheSUyMHNreXxlbnwwfHwwfHx8MA%3D%3D')" 
+    }else if(dataobject.current.is_day == 1) {
+      document.querySelector(".container").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG')"
+    }
 })
 
 .catch((error) => {
-  console.log(error);
+console.log(error);
 });
+}
+)
 
 citySearchInput.addEventListener("keypress", async (key) => {
   if (key.key === "Enter") {
@@ -68,6 +78,14 @@ citySearchInput.addEventListener("keypress", async (key) => {
 
     .then((dataobject) => {
 
+      document.querySelector("header").style.marginTop = 0;
+
+      document.querySelector(".weather-info").style.display = "flex"
+
+      document.querySelector(".container").style.width = "400px"
+
+      document.querySelector(".container").style.height = "460px"
+
       document.querySelector("#locname").textContent = dataobject.location.name;
 
       document.querySelector("#mainimg").src = dataobject.current.condition.icon;
@@ -81,28 +99,35 @@ citySearchInput.addEventListener("keypress", async (key) => {
       let forecasttemp = document.querySelectorAll(".forecasttemp");
       let forecasttime = document.querySelectorAll(".forecasttime");
       let forecastimg = document.querySelectorAll(".forecastimg");
+      let currTime = Number(dataobject.current.last_updated.slice(11, 13))
+
+      forecasttime.forEach((element) => {
+        for (let i = 0; i < 4; i++) {
+          let forTime = String(currTime + 1 + i);
+          forecasttime[i].textContent = forTime;
+          }
+        }),
 
       forecasttemp.forEach((element) => {
         for (let i = 0; i < 4; i++) {
           forecasttemp[i].textContent =
-            Math.round(dataobject.forecast.forecastday[0].hour[i].temp_c) + "°";
+            Math.round(dataobject.forecast.forecastday[0].hour[currTime + 1 + i].temp_c) + "°";
           
-          forecastimg[i].src = dataobject.forecast.forecastday[0].hour[i].condition.icon;
+          forecastimg[i].src = dataobject.forecast.forecastday[0].hour[currTime + 1 + i].condition.icon; 
         }
       });
 
-      forecasttime.forEach((element) => {
-        for (let i = 0; i < 4; i++) {
-          forecasttime[i].textContent = dataobject.forecast.forecastday[0].hour[
-            i
-          ].time.slice(11, 13);
-        }
-      });
+      if(dataobject.current.is_day == 0) {
+        document.querySelector(".container").style.backgroundImage = "url('Images/nightSkybg.avif')"
+      }else if(dataobject.current.is_day == 1 && dataobject.current.condition.text.includes("rain", "snow")) {
+        document.querySelector(".container").style.backgroundImage = "url('https://images.unsplash.com/photo-1595949389862-dcc5a5945217?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JheSUyMHNreXxlbnwwfHwwfHx8MA%3D%3D')" 
+      }else if(dataobject.current.is_day == 1) {
+        document.querySelector(".container").style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG')"
+      }
 })
 
-    .catch((error) => {
-      console.log(error);
-    })
+.catch((error) => {
+  console.log(error);
+});
   }
-})
 })
